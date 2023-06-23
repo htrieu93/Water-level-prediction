75965a42f874a6d7af6132472eeadf9f0594bcf3
Hieu Trieu <htrieu93@gmail.com>
2023-06-23 23:39:24 +0700

    Create metrics.py

---
+++ code/metrics.py
@@ -0,0 +1,14 @@
+from sklearn.metrics import r2_score, mean_absolute_error, max_error, mean_squared_error
+import tensorflow as tf
+
+def get_max_error(y_test, y_pred):
+    return abs(y_test - y_pred).max().values[0]
+
+def calculate_loss(y_test, y_pred):
+    max_val_error = get_max_error(y_test, y_pred)
+
+    # calculate LOSS
+    rmse = np.round_(float(mean_squared_error(y_test, y_pred, squared=False)), 3)
+    r2 = np.round_(float(r2_score(y_test, y_pred)), 3)
+    mae = np.round_(float(mean_absolute_error(y_test, y_pred)), 3)
+    return r2, rmse, mae, max_val_error
