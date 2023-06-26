551b4453f06d1003986be505f0bb2d38e8774388
Hieu Trieu <htrieu93@gmail.com>
2023-06-27 00:10:02 +0700

    Create preprocess.py

---
+++ src/preprocess.py
@@ -0,0 +1,6 @@
+
+
+DATA_2010_PATH = '../data/XQHh_Nhatle_2010.xlsx'
+DATA_2012_PATH = '../data/XQHh_Nhatle_2012.xlsx'
+DATA_2016_PATH = '../data/XQHh_Nhatle_2016.xlsx'
+DATA_2020_PATH = '../data/XQHh_Nhatle_2020.xlsx'
